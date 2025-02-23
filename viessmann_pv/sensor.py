d4c7f0904496a6d47f7c8dfa666e2984383e2815
markusschultheis <m.schultheis@icloud.com>
2025-02-23 12:52:08 -0500

    Update sensor.py

--- viessmann_pv/sensor.py
+++ viessmann_pv/sensor.py
@@ -1,12 +1,12 @@
-from homeassistant.core import HomeAssistant
+rom homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from .gridx_api import GridXAPI
-from .entities import ViessmannSensor 
+from .entities import ViessmannSensor
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
     """Set up GridX sensors from a config entry."""
-    
+
     # GridX API-Client erstellen
     api = GridXAPI(
         entry.data["username"],
@@ -22,18 +22,19 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
 
     # Sensoren erstellen
     sensors = [
-        ViessmannSensor(api, "PV Power", "photovoltaic"),
-        ViessmannSensor(api, "Grid Power", "grid"),
-        ViessmannSensor(api, "Battery Charge", "stateOfCharge"),
-        ViessmannSensor(api, "Remaining Charge", "remainingCharge"),
-        ViessmannSensor(api, "Direct Consumption", "directConsumption"),
-        ViessmannSensor(api, "Consumption", "consumption"),
-        ViessmannSensor(api, "Total Consumption", "totalConsumption"),
-        ViessmannSensor(api, "Production", "production"),
-        ViessmannSensor(api, "Self Supply", "selfSupply"),
-        ViessmannSensor(api, "Self Suffieciency Rate", "selfSufficiencyRate"),
-        ViessmannSensor(api, "Grid Meter Reading Positiv", "gridMeterReadingPositive"),
-        ViessmannSensor(api, "Grid Meter Reading Negativ", "gridMeterReadingNegative")
+        ViessmannSensor(api, "PV Power", "Wh", "photovoltaic", "Viessmann_photovolatic", "energy"),
+        ViessmannSensor(api, "Grid Power", "Wh", "grid", "Viessmann_grid", "energie"),
+        ViessmannSensor(api, "State of Charge", "%", "battery.stateOfCharge", "Viessmann_stateOfCharge", ""),
+        ViessmannSensor(api, "Remaining Charge", "Wh", "battery.remainingCharge", "Viessmann_remainingCharge", ""),
+        ViessmannSensor(api, "Consumption", "Wh", "consumption", "Viessmann_consumption", "energy"),
+        ViessmannSensor(api, "Direct Consumption", "Wh", "directConsumption", "Viessmann_directConsumption", "energy"),
+        ViessmannSensor(api, "Direct Consumption household", "Wh", "directConsumptionHousehold", "Viessmann_HhConsumption", "energy"),
+        ViessmannSensor(api, "Total Consumption", "W", "totalConsumption", "Viessmann_totalConsumption", "energy"),
+        ViessmannSensor(api, "Production", "Wh", "production", "Viessmann_production", "energy"),
+        ViessmannSensor(api, "Self Supply", "Wh", "selfSupply", "Viessmann_selfSupply", "energy"),
+        ViessmannSensor(api, "Self Suffieciency Rate", "", "selfSufficiencyRate", "Viessmann_selfSufficencyRate", ""),
+        ViessmannSensor(api, "Grid Meter Reading Positiv", "Wh", "gridMeterReadingPositive", "Viessmann_gridMeterPos", ""),
+        ViessmannSensor(api, "Grid Meter Reading Negativ", "Wh", "gridMeterReadingNegative", "Viessmann_gridMeterNeg", "")
     ]
-    
+
     async_add_entities(sensors, update_before_add=True)
