f403f5ddf58d54e14839e76db8f1ecc566df9169
markusschultheis <m.schultheis@icloud.com>
2025-09-05 19:08:33 -0400

    Update const.py
    Added Const

--- viessmann_pv/const.py
+++ viessmann_pv/const.py
@@ -1,5 +1,7 @@
-DOMAIN = "viessmann_pv"
+DOMAIN = "gridx"
 
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
 CONF_CLIENT_ID = "client_id"
 CONF_REALM = "realm"
 CONF_AUDIENCE = "audience"
@@ -7,3 +9,12 @@ CONF_AUDIENCE = "audience"
 AUTH_URL = "https://gridx.eu.auth0.com/oauth/token"
 GATEWAYS_URL = "https://api.gridx.de/gateways"
 LIVE_URL = "https://api.gridx.de/systems/{}/live"
+
+GRANT_TYPE = "http://auth0.com/oauth/grant-type/password-realm"
+
+# Schlüssel für gespeicherte Tokens
+DATA_ACCESS_TOKEN = "access_token"
+DATA_REFRESH_TOKEN = "refresh_token"
+DATA_EXPIRES_AT = "expires_at"
+DATA_GATEWAY_ID = "gateway_id"
+DATA_ID_TOKEN = "id_token"
