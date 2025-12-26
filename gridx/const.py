682e239735116cc68c980722af6b1ded36ae5e96
markusschultheis <m.schultheis@icloud.com>
2025-12-26 14:25:09 -0500

    Refactor constants in const.py
    Removed username and password constants, added token expiration offset.

--- gridx/const.py
+++ gridx/const.py
@@ -1,7 +1,5 @@
 DOMAIN = "gridx"
 
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
 CONF_CLIENT_ID = "client_id"
 CONF_REALM = "realm"
 CONF_AUDIENCE = "audience"
@@ -12,9 +10,9 @@ LIVE_URL = "https://api.gridx.de/systems/{}/live"
 
 GRANT_TYPE = "http://auth0.com/oauth/grant-type/password-realm"
 
-# Schlüssel für gespeicherte Tokens
-DATA_ACCESS_TOKEN = "access_token"
-DATA_REFRESH_TOKEN = "refresh_token"
+# Token expiration offset (seconds before actual expiration to refresh)
+TOKEN_EXPIRATION_OFFSET = 52200
+
+# Data storage keys
 DATA_EXPIRES_AT = "expires_at"
-DATA_GATEWAY_ID = "gateway_id"
 DATA_ID_TOKEN = "id_token"
